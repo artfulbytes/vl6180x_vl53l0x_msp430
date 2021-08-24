69ccf734da42058b770b031fa851a285fbc9ba7f
Niklas Nilsson <niklas@artfulbytes.com>
2021-08-24 06:10:09 +0200

    ENH: drivers/i2c: Support for different address and data sizes
    I2C devices may have registers with different address and data sizes,
    so it makes sense to add support for that. For example, VL6180X is
    has 16-bit address registers with data sizes 8/16/32-bits. While
    VL53L0X is 8-bit addressed with data sizes 8/16/32-bits.
    
    Create two general functions for reading and writing a register and
    create wrappers around them for specific combinations of addr and
    data sizes. Also, create two functions for reading and writing an
    array of bytes.

--- drivers/i2c.h
+++ drivers/i2c.h
@@ -5,7 +5,44 @@
 #include <stdbool.h>
 
 void i2c_init(void);
+void i2c_set_slave_address(uint8_t addr);
+
+/**
+ * Wrapper functions for reading from registers with different address
+ * and data sizes.
+ * @return True if success, False if error
+ * NOTE: Reads the most significant byte first if multi-byte data.
+ * NOTE: Polling-based
+ */
+bool i2c_read_addr8_data8(uint8_t addr, uint8_t *data);
+bool i2c_read_addr8_data16(uint8_t addr, uint16_t *data);
 bool i2c_read_addr16_data8(uint16_t addr, uint8_t *data);
+bool i2c_read_addr16_data16(uint16_t addr, uint16_t *data);
+bool i2c_read_addr8_data32(uint16_t addr, uint32_t *data);
+bool i2c_read_addr16_data32(uint16_t addr, uint32_t *data);
+
+/**
+ * Read byte_count bytes from address addr
+ * NOTE: Polling-based
+ */
+bool i2c_read_addr8_bytes(uint8_t start_addr, uint8_t *bytes, uint16_t byte_count);
+
+/**
+ * Wrapper functions for writing to registers with different address
+ * and data sizes.
+ * @return True if success, False if error
+ * NOTE: Writes the most significant byte if multi-byte data.
+ * NOTE: Polling-based
+ */
+bool i2c_write_addr8_data8(uint8_t addr, uint8_t data);
+bool i2c_write_addr8_data16(uint8_t addr, uint16_t data);
 bool i2c_write_addr16_data8(uint16_t addr, uint8_t data);
+bool i2c_write_addr16_data16(uint16_t addr, uint16_t data);
+
+/**
+ * Write an array of bytes of size byte_count to address addr.
+ * NOTE: Polling-based
+ */
+bool i2c_write_addr8_bytes(uint8_t start_addr, uint8_t *bytes, uint16_t byte_count);
 
 #endif /* I2C_H */
